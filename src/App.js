43f40c9d89ecbbbc254409cba630942f8e4534ed
alexvcs <alexcurtisslep@gmail.com>
2022-10-08 12:22:36 -0400

    working on the card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Navbar />
+      <Card />
     </div>
   );
 }
