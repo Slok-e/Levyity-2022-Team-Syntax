457d1320f01c348469f46617fb6036268220e349
Slok-e <106457601+Slok-e@users.noreply.github.com>
2022-10-08 22:20:43 -0400

    rest framework

--- backend/hollywoodcentralhub/models.py
+++ backend/hollywoodcentralhub/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+class parkreservation(models.Model):
+    image = models.ImageField()
+    parkname = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    pavillion = models.IntegerField(range(1,20))
