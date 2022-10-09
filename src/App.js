b28f7b305ca684ece10cb66d04ac0f332ef91974
dlongo <dlongodesign@gmail.com>
2022-10-09 06:09:30 -0400

    add permit and service request content

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Live from "./components/Live";
 import Play from "./components/Play";
 import Work from "./components/Work";
 import Invest from "./components/Invest";
 import TopNav from "./components/TopNav";
-import { Grid, Stack } from "@mui/material";
+import { Grid } from "@mui/material";
 import Sidebar from "./components/SideNav";
 
 function App() {
   return (
     <Grid container className="w-screen bg-slate-200" alignItems={"stretch"}>
       <TopNav />
-      <Grid item sm={3} style={{ height: "100%" }}>
+      <Grid item sm={2} style={{ height: "100%" }}>
         <Sidebar />
       </Grid>
-      <Grid item sm={9} my={2} style={{ height: "100%", padding: "1rem 1rem 1rem 0" }}>
+      <Grid item sm={10} my={2} style={{ height: "100%", padding: "1rem 1rem 1rem 0" }}>
         <Routes>
           <Route path={"/dashboard"} element={<Dashboard />} />
           <Route path={"/live"} element={<Live />} />
           <Route path={"/play"} element={<Play />} />
           <Route path={"/work"} element={<Work />} />
           <Route path={"/invest"} element={<Invest />} />
-          {/* <div>{cards}</div> */}
         </Routes>
       </Grid>
     </Grid>
