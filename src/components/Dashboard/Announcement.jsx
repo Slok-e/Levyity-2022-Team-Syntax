50fb7d0dc48d225372e6c54dac70b772d9d4c0f9
dlongo <dlongodesign@gmail.com>
2022-10-09 07:03:53 -0400

    add content on live route

--- src/components/Dashboard/Announcement.jsx
+++ src/components/Dashboard/Announcement.jsx
@@ -1,14 +1,7 @@
 import * as React from "react";
-import PropTypes from "prop-types";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import {
-  Circle,
-  KeyboardArrowLeft,
-  KeyboardArrowRight,
-} from "@mui/icons-material";
+import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
 import { Button, MobileStepper, Paper } from "@mui/material";
 import { useTheme } from "@emotion/react";
 import SwipeableViews from "react-swipeable-views";
@@ -56,37 +49,6 @@ export default function BasicTabs() {
   };
 
   return (
-    // <Box sx={{ width: "100%", padding: "1rem" }}>
-    //   <Typography variant="h6" component="h2" fontWeight={700}>
-    //     Announcements
-    //   </Typography>
-    //   <TabPanel value={value} index={0}>
-    //     <h3>Hollywood Features Many Historic Homes</h3>
-    //     <p>
-    //       The Downtown Hollywood Mural Program features more than 30 must-see
-    //       murals by world class artists
-    //     </p>
-    //   </TabPanel>
-    //   <TabPanel value={value} index={1}>
-    //     <h3>Hollywood Plans to Open Park</h3>
-    //     <p>Construction starts next year</p>
-    //   </TabPanel>
-    //   <TabPanel value={value} index={2}>
-    //     <h3>Building Permit Approved</h3>
-    //     <p>Amid public outcry the building will go up</p>
-    //   </TabPanel>
-    //   <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-    //     <Tabs
-    //       value={value}
-    //       onChange={handleChange}
-    //       aria-label="basic tabs example"
-    //     >
-    //       <Tab icon={<Circle />} {...a11yProps(0)} />
-    //       <Tab className="tab-icon" icon={<Circle />} {...a11yProps(1)} />
-    //       <Tab icon={<Circle />} {...a11yProps(2)} />
-    //     </Tabs>
-    //   </Box>
-    // </Box>
     <Box sx={{ maxWidth: "100%", flexGrow: 1 }}>
       <Paper
         elevation={3}
